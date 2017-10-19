8a6b415a69bb985d751651667043a0aa4139da9a
Jakub Kulík <kulikjak@gmail.com>
2017-10-19 14:50:38 +0200

    Initial commit

---
+++ src/routes.php
@@ -0,0 +1,16 @@
+<?php
+// Routes
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app->get('/', function(Request $request, Response $response) {
+    return $this->view->render($response, "index.phtml");
+});
+
+$app->get('/md[/{route:.+}]', function (Request $request, Response $response, $args) {
+    // $params is an array of all the optional segments
+    $route = $request->getAttribute('route');
+    $response = $this->view->render($response, "markdown.phtml", ["file" => $route, "title" => "Tutorials"]);
+    return $response;
+});
